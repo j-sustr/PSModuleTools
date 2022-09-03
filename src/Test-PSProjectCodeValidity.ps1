07e74a41aaf6d36e23778b6af0d79d96b3a3ae04
Jan Šustr <jan.sustr@unicorn.com>
2022-09-03 15:21:13 +0200

    comments

--- src/Test-PSProjectCodeValidity.ps1
+++ src/Test-PSProjectCodeValidity.ps1
@@ -1,3 +1,7 @@
 
 
 # TODO: Check if public funtion's name in a file corresponds with the filename
+
+# TODO: Check if *.psd1 and .psm1 files are up to date with the code
+
+# TODO: Call this func during install to ensure project is updated
