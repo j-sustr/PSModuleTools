e2455e2a0a77b3883bfd82daa6c7c0681ac06c66
Jan Šustr <jan.sustr@unicorn.com>
2022-09-04 17:00:37 +0200

    update module files

--- src/PSModuleTools.psm1
+++ src/PSModuleTools.psm1
@@ -1,13 +1,13 @@
-. $PSScriptRoot\Assert-PSModuleProjectFiles.ps1
-. $PSScriptRoot\Get-PSCodeInfo.ps1
-. $PSScriptRoot\Get-PSModulePathForCurrentUser.ps1
-. $PSScriptRoot\Get-PSModuleProjectCodeInfo.ps1
-. $PSScriptRoot\Initialize-PSModuleProject.ps1
-. $PSScriptRoot\Install-ModuleForCurrentUser.ps1
-. $PSScriptRoot\Test-PSProjectCodeValidity.ps1
-. $PSScriptRoot\Update-PSModuleProjectFiles.ps1
-. $PSScriptRoot\utils\assertRepoHasNoWorking.ps1
-. $PSScriptRoot\utils\getRepoRoot.ps1
-. $PSScriptRoot\utils\getSrcRoot.ps1
 . $PSScriptRoot\utils\promptYesNo.ps1
+. $PSScriptRoot\utils\getSrcRoot.ps1
+. $PSScriptRoot\utils\getRepoRoot.ps1
+. $PSScriptRoot\utils\assertRepoHasNoWorking.ps1
+. $PSScriptRoot\Update-PSModuleProjectFiles.ps1
+. $PSScriptRoot\Test-PSProjectCodeValidity.ps1
+. $PSScriptRoot\Install-ModuleForCurrentUser.ps1
+. $PSScriptRoot\Initialize-PSModuleProject.ps1
+. $PSScriptRoot\Get-PSModuleProjectCodeInfo.ps1
+. $PSScriptRoot\Get-PSModulePathForCurrentUser.ps1
+. $PSScriptRoot\Get-PSCodeInfo.ps1
+. $PSScriptRoot\Assert-PSModuleProjectFiles.ps1
 
